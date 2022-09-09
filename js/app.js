56390c3499c4bcc75bd8e602e705d479ce3990eb
juan rosas <juan.rosas.andrade@gmail.com>
2022-09-09 16:58:35 -0300

    se valida funcion para remplazar letras por guiones y viceversa

--- js/app.js
+++ js/app.js
@@ -1,19 +1,37 @@
-const palabras = ["caballo", "perro", "gato", "mesa", "silla", "computador", "luna", "oscuridad", "automovil", "avion"];
+let vidas = 9;
+const palabras = ["caballo", "perro", "gato", "mesa", "silla", "computador", "luna", "oscuridad", "automovil", "avion", "sofa", "telefono", "pantalla", "lapiz", "escritorio"];
+const expresionReg = /^[a-z]+$/i;
 
-function palabraAzar(lista) {
-    let index = Math.floor(Math.random() * lista.length);
-    // console.log(index)
-    dibujarGuiones(lista[index])
-    return lista[index];
+const inciarJuego = () => {
+    let palabraAzar = palabraAleatoria(palabras);
+    funRemplazarLetras(palabraAzar)
 }
 
-palabraAleatoria = palabraAzar(palabras)
-console.log(palabraAleatoria)
+const palabraAleatoria = (lista) => {
+    let index = Math.floor(Math.random() * lista.length);
+    console.log(lista[index])
+    palabraSelecionada = lista[index];
+    return palabraSelecionada
+}
 
-function dibujarGuiones(palabraSorteada) {
-    let palabraConvertida = palabraSorteada.replace(/[A-Za-z]/gm, "_ ");
-    document.querySelector("#salida").value = palabraConvertida;
+const funRemplazarLetras = (palabra) => {
+    let palabraConvertida = palabra.replace(/[A-Za-z]/gm, "_");
+    document.querySelector("#salida").value = palabraConvertida
+    const arrayLetras = Array.from(palabraConvertida);
+    document.addEventListener("keypress", (e) => {
+        for (let i = 0; i < palabra.length; i++) {
+            letraPresionada = e.key;
+            if (letraPresionada == palabra[i]) {
+                console.log(palabra[i]);
+                arrayLetras[i] = letraPresionada
+            }
+        }
+        console.log(arrayLetras)
+        let cadena = arrayLetras.join('');
+        document.querySelector("#salida").value = cadena
+    })
 }
 
 const botonJugar = document.getElementById("boton-comenzar");
-botonJugar.addEventListener("click", palabraAzar)
+console.log(botonJugar)
+botonJugar.addEventListener("click", inciarJuego)
